5532f40ae9872cae9b70d0cfbf7db758e0530bfb
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-05-28 12:09:31 +0200

    Configure server to use routes

--- src/passport/passport.config.js
+++ src/passport/passport.config.js
@@ -3,12 +3,4 @@ require('./strategies/local.strategy')();
 
 module.exports = function passportConfig(server) {
   server.use(passport.initialize());
-
-  passport.serializeUser((user, done) => {
-    done(null, user);
-  });
-
-  passport.deserializeUser((user, done) => {
-    done(null, user);
-  });
 };
